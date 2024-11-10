8611657eede45517d7a0cbc329fc078106c9e34e
Basat maqsood <itzbasatmaqsood@gmail.com>
2024-11-10 15:45:36 +0500

    links fixed

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
 import React from 'react';
 
 function Footer() {
   return (
     <div className="bg-black text-white py-8 px-8" >
-      <a href="/"><h1 className="text-3xl font-bold">Swift<span className='text-yellow-400'>!</span></h1></a>
+      <Link href="/"><h1 className="text-3xl font-bold">Swift<span className='text-yellow-400'>!</span></h1></Link>
     </div>
   );
 }
